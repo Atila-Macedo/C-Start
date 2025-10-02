4b3bdb12de4dc733e09bedc0abe57b792a4a393a
Atila-Macedo <dev.atila0110@hotmail.com>
2025-10-01 20:54:06 -0400

    docs: Variáveis e I/o

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,5 +1,46 @@
 ﻿#region Meu primeiro programa em C#
-    
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+#endregion
+
+#region Tipos de Váriaveis
+
+// Insetir Dados dentro de uma variável
+
+#region Variáveis de entrada e Saída
+Console.WriteLine("Digite sua idade: "); // Imprime no console
+string idadeImput = Console.ReadLine(); // Lê o que o usuário digitou no console
+Console.WriteLine($"Sua idade é {idadeImput} anos");// Imprime no console a idade digitada pelo usuário
+#endregion
+
+#region Variáveis de tipos primitivos
+
+/* // Texto (Conjunto de caracteres)
+string nome = "Átila";
+int idade = 25; 
+Console.WriteLine($"Seu nome é {nome}"); // Interpolação de strings
+Console.WriteLine("Seu nome é {0} e tem {1} anos.", nome, idade); // Placeholders */
+
+#endregion
+
+#region Declaração de Variáveis
+/* // Sintaxe de Decclaração de Variáveis
+// Do tipo Inteiro
+int idade; // posso começar declarando a variável sem atribuir um valor
+int idade02 = 25; // posso declarar e atribuir um valor na mesma linha
+
+//Numero quebrado (numero com parte fracionária)
+double peso; // posso começar declarando a variável sem atribuir um valor
+double altura = 1.77; // Números quebrados com mais precisão (Exemplo: 10.5, 25.99, -1500.50)
+double altura02 = 1.77; // Números quebrados com menos precisão
+
+// Texto (Conjunto de caracteres)
+string nome; // Exemplo: "João", "Maria", "Pedro"
+
+// Booleano (Verdadeiro ou Falso)
+bool estudante; // Exemplo: true (verdadeiro), false (falso)
+bool estudante02 = true;
+bool estudante03 = false;
+bool valor = 2 < 3; */
+#endregion
 
 #endregion
