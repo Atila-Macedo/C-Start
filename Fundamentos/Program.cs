587b1f40b963f383491c04bf56a320f5f3724127
Atila-Macedo <dev.atila0110@hotmail.com>
2025-10-03 21:01:05 -0400

    docs: Variáveis, OA

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -8,9 +8,9 @@
 // Insetir Dados dentro de uma variável
 
 #region Variáveis de entrada e Saída
-Console.WriteLine("Digite sua idade: "); // Imprime no console
+/* Console.WriteLine("Digite sua idade: "); // Imprime no console
 string idadeImput = Console.ReadLine(); // Lê o que o usuário digitou no console
-Console.WriteLine($"Sua idade é {idadeImput} anos");// Imprime no console a idade digitada pelo usuário
+Console.WriteLine($"Sua idade é {idadeImput} anos");// Imprime no console a idade digitada pelo usuário */
 #endregion
 
 #region Variáveis de tipos primitivos
@@ -43,5 +43,43 @@ bool estudante02 = true;
 bool estudante03 = false;
 bool valor = 2 < 3; */
 #endregion
+
+#endregion
+
+//Aula 03 - Operadores Aritméticos, Atribuição e Comparação
+#region Operadores Aritméticos 
+
+#region Exercício 1
+/* string nomeDoUsuario;
+int idadeDoUsuario;
+
+Console.Write("Digite seu nome: ");
+nomeDoUsuario = Console.ReadLine()!;
+Console.Write("Digite sua idade: ");
+idadeDoUsuario = int.Parse(Console.ReadLine()!);
+Console.Write($"Olá {nomeDoUsuario}, você tem {idadeDoUsuario} anos."); */
+#endregion
+
+#region  Exercício 2
+/* int num1 = 10, num2 = 20; 
+Console.WriteLine($"A soma entre {num1} e {num2} é igual a {num1 + num2}.");  
+Console.WriteLine($"A subtração entre {num2} e {num1} é igual a {num2 - num1}.");
+Console.WriteLine($"A multiplicação entre {num1} e {num2} é igual a {num1 * num2}."); 
+Console.WriteLine($"A divisão entre {num2} e {num1} é igual a {num2 / num1}.");
+
+int num3 = 11, num4 = 3;
+Console.WriteLine($"O resto da divisão entre {num3} e {num4} é igual a {num3 % num4}."); */
+#endregion
+
+#region  Exercício 3
+/* double raio = 5.5;
+Console.WriteLine($"Para um círculo com raio {raio}, a área é {Math.PI * Math.Pow(raio, 2)} e o perímetro é {2 * Math.PI * raio}."); */
+#endregion
+
+#region Exercício 4
+/* double c = 25.0;
+Console.WriteLine($"{c}°C equivalem a {(c * 9/5) + 32}°F."); */
+
+ #endregion
 
 #endregion
